c94edd4aa031f9a0805d2c11b47aaf19c670f8c2
iallbeback <146027288+iallbeback@users.noreply.github.com>
2024-10-19 20:58:44 +0000

    Add files via upload

--- task 4/prak4.c
+++ task 4/prak4.c
@@ -112,6 +112,23 @@ void matmul_sequential(double* A, double* B, double* C, int N) {
     }
 }
 
+void matmul_diagonal(double* A, double* B, double* C, int N) {
+    for (int d = 0; d < 2 * N - 1; d++) {
+        int i_start = (d < N) ? 0 : d - N + 1;
+        int i_end = (d < N) ? d : N - 1;
+
+        for (int i = i_start; i <= i_end; i++) {
+            int j = d - i;
+
+            double sum = 0.0;
+            for (int k = 0; k < N; k++) {
+                sum += A[i * N + k] * B[k * N + j];
+            }
+            C[i * N + j] = sum;
+        }
+    }
+}
+
 void transpose(double* B, double* B_T, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -170,15 +187,16 @@ void matmul_avx_d(double* A, double* B, double* B_T, double* C, int N) {
 }
 
 void benchmark(int N) {
-    double *A, *B, *B_T, *B_T_d, *C_seq, *C_avx, *C_avx_d;
+    double *A, *B, *B_T, *B_T_d, *C_seq, *C_avx, *C_avx_d, *C_diag;
 
     int r = posix_memalign((void**)&A, 32, N * N * sizeof(double));
     r = posix_memalign((void**)&B, 32, N * N * sizeof(double));
-	r = posix_memalign((void**)&B_T, 32, N * N * sizeof(double));
-	r = posix_memalign((void**)&B_T_d, 32, N * N * sizeof(double));
+    r = posix_memalign((void**)&B_T, 32, N * N * sizeof(double));
+    r = posix_memalign((void**)&B_T_d, 32, N * N * sizeof(double));
     r = posix_memalign((void**)&C_seq, 32, N * N * sizeof(double));
     r = posix_memalign((void**)&C_avx, 32, N * N * sizeof(double));
-	r = posix_memalign((void**)&C_avx_d, 32, N * N * sizeof(double));
+    r = posix_memalign((void**)&C_avx_d, 32, N * N * sizeof(double));
+    r = posix_memalign((void**)&C_diag, 32, N * N * sizeof(double));
 
     init_matrix(A, N);
     init_matrix(B, N);
@@ -188,39 +206,49 @@ void benchmark(int N) {
     double end_seq = omp_get_wtime();
     double time_taken_seq = end_seq - start_seq;
     printf("N = %d, seq mull:\t %f secs\n", N, time_taken_seq);
+	
+	double start_diag = omp_get_wtime();
+    matmul_diagonal(A, B, C_diag, N);
+    double end_diag = omp_get_wtime();
+    double time_taken_diag = end_diag - start_diag;
+    printf("N = %d, diag mull:\t %f secs\n", N, time_taken_diag);
 
     double start_avx = omp_get_wtime();
     matmul_avx(A, B, B_T, C_avx, N);
     double end_avx = omp_get_wtime();
     double time_taken_avx = end_avx - start_avx;
     printf("N = %d, AVX mull:\t %f secs\n", N, time_taken_avx);
-	
-	double start_avx_d = omp_get_wtime();
+
+    double start_avx_d = omp_get_wtime();
     matmul_avx_d(A, B, B_T_d, C_avx_d, N);
     double end_avx_d = omp_get_wtime();
     double time_taken_avx_d = end_avx_d - start_avx_d;
     printf("N = %d, AVX diag mull:\t %f secs\n", N, time_taken_avx_d);
-	
-	if(N==8) {
-		print_matrices_and_diff_to_file(C_seq, C_avx, N, "matrices_and_diff.txt");
-		print_matrices_and_diff_to_file_d(C_seq, C_avx_d, N, "matrices_and_diff.txt");
-	}
-	
-	if(are_matrices_equal(C_seq, C_avx, N) && are_matrices_equal(C_seq, C_avx_d, N)){
-		printf("equal\n");
-	}
-	else {
-		printf("not equal!!!\n");
-	}
+
+    if (N == 8) {
+		print_matrices_and_diff_to_file(C_seq, C_diag, N, "matrices_and_diff.txt");
+        print_matrices_and_diff_to_file_d(C_seq, C_avx, N, "matrices_and_diff.txt");
+        print_matrices_and_diff_to_file_d(C_seq, C_avx_d, N, "matrices_and_diff.txt");
+    }
+
+    if (are_matrices_equal(C_seq, C_avx, N) &&
+        are_matrices_equal(C_seq, C_avx_d, N) &&
+        are_matrices_equal(C_seq, C_diag, N)) {
+        printf("equal\n");
+    } else {
+        printf("not equal!!!\n");
+    }
 
     free(A);
     free(B);
-	free(B_T);
-	free(B_T_d);
+    free(B_T);
+    free(B_T_d);
     free(C_seq);
     free(C_avx);
-	free(C_avx_d);
+    free(C_avx_d);
+    free(C_diag);
 }
+
 
 int main() {
     srand(time(NULL));
